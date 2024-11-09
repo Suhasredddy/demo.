a14fd3e7ab4faa7570581328425390e3377883b3
tsr <xyz.gmail.com>
2024-11-09 13:39:09 +0530

    my second commit

--- demo1.sh
+++ demo1.sh
@@ -5,7 +5,8 @@
 echo "Virat Kohli (Hindi pronunciation: [ʋɪˈɾɑːʈ ˈkoːɦli] ⓘ; born 5 November 1988) is an Indian international cricketer who plays Test and ODI cricket for the Indian national team. A former captain in all formats of the game, 
 Kohli has garnered 10 ICC Awards which is the most decorated player in International Cricket history. He won the ICC ODI Player of the Year award four times in 2012, 2017, 2018, and 2023. He also won the Sir Garfield Sobers Trophy, given to the ICC Cricketer of the Year, on two occasions, in 2017 and 2018 respectively. In 2018, he became the first player to win both ICC ODI and Test Player of the Year awards in the same year. Also, he was named the Wisden Leading Cricketer in the World for three consecutive years, from 2016 to 2018. Kohli has the second most and most 'Player of the Match' and 'Player of the Series' awards to his name, respectively, in all three formats combined. At the national level, Kohli was honoured with the Arjuna Award in 2013, the Padma Shri in 2017, and India's highest sporting honour, the Khel Ratna Award, in 2018. In 2018, Time magazine included him on its list of the 100 most influential people in the world.
 
-After winning the 2024 T20 World Cup and the Player of the Match in the Final, Kohli announced his retirement from T20Is.]"
+After winning the 2024 T20 World Cup and the Player of the Match in the Final, Kohli announced his retirement from T20Is.]
+      done by suhas "
 ~
 ~
 
